5763cc857c048e093a2212c07a9525b71dd97921
NewageStriker <newagestriker@gmail.com>
2020-11-07 21:16:37 +0530

    add nav v1

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -3,15 +3,20 @@ import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
+import { NavComponent } from './nav/nav.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
    declarations: [
       AppComponent,
-      ValueComponent
+      ValueComponent,
+      NavComponent
    ],
    imports: [
       BrowserModule,
-      HttpClientModule
+      HttpClientModule,
+      FormsModule
+
    ],
    providers: [],
    bootstrap: [
